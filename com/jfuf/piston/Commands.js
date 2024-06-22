52fad95f6404755b8351c758d0924949843ba7d0
jfuf <160429113+jfufff@users.noreply.github.com>
2024-06-22 02:25:33 -0700

    Commands.js

---
+++ com/jfuf/piston/Commands.js
@@ -0,0 +1,11 @@
+/* com.jfuf.piston.Commands */
+/* author: jfuf @ Kron Studios (https://gihub.com/jfufff) */
+/* date of addition: 6/21/24 */
+
+command('stat', function (player, args) {
+   if (args == "deaths") {
+     // !!!TODO: Find in the API Docs somewhere documenting saying something, otherwise ill just have to implement a bot or some 
+     // shit like that :(
+   }
+});
+
