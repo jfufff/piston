f288235c6d1f11861bfe2190ce3f2fac3c1f53b0
jfuf <160429113+jfufff@users.noreply.github.com>
2024-06-22 00:24:44 -0700

    Optimizations #1

--- com/jfuf/piston/Plugin.ts
+++ com/jfuf/piston/Plugin.ts
@@ -1,7 +1,8 @@
 /* com.jfuf.piston.Piston */
 /* author: jfuf @ Kron Studios (https://gihub.com/jfufff) */
 /* date of addition: 6/21/24 */
-type package = { file_name:string, packageError => () {   
+const package = "com.jfuf.piston.Plugin";
+function packageError() {   
  if (!package == "com.jfuf.piston.Plugin") { 
   console.log(
     "piston: warning: malformed package name \n"
